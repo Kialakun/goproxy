978ddd3e2f2fb49e79c34c910682f624073e1367
K!a <kialakunngalgal@gmail.com>
2022-07-08 15:12:15 +1000

    Create cmd.go

---
+++ cmd.go
@@ -0,0 +1,28 @@
+package goproxy
+
+import (
+	"flag"
+	"os"
+	"os/signal"
+	"syscall"
+
+	log "github.com/sirupsen/logrus"
+)
+
+var PORT = os.Getenv("PORT")
+
+func CMD() {
+	log.SetFormatter(&log.TextFormatter{ForceColors: true})
+
+	var port string
+	flag.StringVar(&port, "port", "8888", "Proxy listen port")
+	var rate int
+	flag.IntVar(&rate, "rate", 512*1024, "Proxy bandwidth ratelimit")
+
+	flag.Parse()
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
+	go Listen(PORT, rate, shutdown)
+	<-shutdown
+}
